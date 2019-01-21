92730da3ced91336a6d3d1256182b4d7edb1bcbd
unknown <2160804@labinfo.is.escuelaing.edu.co>
2019-01-21 09:20:14 -0500

    ejercicio F

--- laboratorios/1-recursion/F/problem.py
+++ laboratorios/1-recursion/F/problem.py
@@ -2,8 +2,11 @@ import json
 
 
 # TODO Complete!
-def hanoi(n):
-    return []
+def hanoi(n, origen, auxiliar, destino):
+    if n > 0: 
+        hanoi(n-1, origen, destino, auxiliar)
+        print ("Se mueve de torre", origen, 'a torre', destino)
+        hanoi(n-1, auxiliar, origen, destino)
 
 
 if __name__ == '__main__':
@@ -11,5 +14,8 @@ if __name__ == '__main__':
         tests = json.load(f)
         for i, test in enumerate(tests):
             n = test["n"]
-            hanoi(n)
+            origen='A'
+            auxiliar='B'
+            destino='C'
+            hanoi(n, origen, auxiliar, destino)
         print('OK!')
