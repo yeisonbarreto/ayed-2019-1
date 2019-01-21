ea17cac1307d22af5e79176db92e6a53207719e2
unknown <2160804@labinfo.is.escuelaing.edu.co>
2019-01-21 11:41:33 -0500

    ejercicio B

--- laboratorios/1-recursion/B/problem.py
+++ laboratorios/1-recursion/B/problem.py
@@ -2,15 +2,15 @@ import json
 
 
 # TODO Complete!
-def arrange(array, orden):
+def arrange(array, par, impar):
     if len(array) == 0:
-        return orden
+        return par+impar
     else:
         if array[0]%2 == 0:
-            orden.insert(len(orden)//2,array[0])
+            par.append(array[0])
         else:
-            orden.append(array[0])
-        return arrange(array[1:],orden)
+            impar.append(array[0])
+        return arrange(array[1:], par, impar)
 
 
 if __name__ == '__main__':
@@ -18,7 +18,7 @@ if __name__ == '__main__':
         tests = json.load(f)
         for i, test in enumerate(tests):
             numbers = test["numbers"]
-            actual = arrange(numbers, [])
+            actual = arrange(numbers, [] ,[])
             expected = test['result']
             assert actual == expected, f'Test {i} | expected: {expected}, actual: {actual}'
         print('OK!')
