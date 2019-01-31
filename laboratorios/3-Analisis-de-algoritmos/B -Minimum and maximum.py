33144976185868c8f708edaf70a0b587e92d70b6
unknown <2160804@labinfo.is.escuelaing.edu.co>
2019-01-31 08:30:12 -0500

    B -Minimum and maximum

--- laboratorios/3-Analisis-de-algoritmos/B -Minimum and maximum.py
+++ laboratorios/3-Analisis-de-algoritmos/B -Minimum and maximum.py
@@ -1,26 +1,42 @@
 from sys import stdin
-def quick_sort(lista):
-    menor = []
-    mayor = []
-    if len(lista) == 0:
-        return []
+def mergesort(a):
+    if len(a) == 1:
+        return a
     else:
-        pivote = lista[0]
-        for i in range(1,len(lista)):
-            if lista[i] < pivote:
-                menor.append(lista[i])
-            else:
-                mayor.append(lista[i])
-    return quick_sort(mayor) + [pivote] + quick_sort(menor)
+        mid = len(a) // 2
+        izq = mergesort(a[:mid])
+        der = mergesort(a[mid:])
+        a = merge(izq,der)
+    return a
+def merge(izq,der):
+    i = 0
+    d = 0
+    n = len(izq)
+    m = len(der)
+    nueva = []
+    while i < n and d < m:
+        if izq[i] < der[d]:
+            nueva += [izq[i]]
+            i +=1
+        else:
+            nueva += [der[d]]
+            d += 1
+    if i < n:
+        while i < n:
+            nueva += [izq[i]]
+            i += 1
+    if d < m:
+        while d < m:
+            nueva += [der[d]]
+            d += 1
+    return nueva
 def main():
-    print('Numero de elementos!')
-    w = int(input())
-    print('Secuencia de números separados por estacio!')
-    x = list(map(int,stdin.readline().strip().split(' ')))
-    res = quick_sort(x)
+    num = 11
+    x = [5,-2,3,4,10,-6,22,11,7,3,0]
     if len(x) <= 1:
         print(x)
     else:
+        res = mergesort(x)
         r = res[-1] , res[0]
         print(*r)
 main()
