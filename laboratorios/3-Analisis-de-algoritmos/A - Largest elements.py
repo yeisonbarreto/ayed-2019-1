7777fc32c3c8ab61f7bba039462716097a70ad19
unknown <2160804@labinfo.is.escuelaing.edu.co>
2019-01-31 08:20:10 -0500

    A - Largest elements

--- laboratorios/3-Analisis-de-algoritmos/A - Largest elements.py
+++ laboratorios/3-Analisis-de-algoritmos/A - Largest elements.py
@@ -1,24 +1,48 @@
+from datetime import datetime
 from sys import stdin
-def quick_sort(lista):
-    menor = []
-    mayor = []
-    if len(lista) == 0:
-        return []
+def mergesort(a):
+    if len(a) == 1:
+        return a
     else:
-        pivote = lista[0]
-        for i in range(1,len(lista)):
-            if lista[i] < pivote:
-                menor.append(lista[i])
-            else:
-                mayor.append(lista[i])
-    return quick_sort(mayor) + [pivote] + quick_sort(menor)
+        mid = len(a) // 2
+        izq = mergesort(a[:mid])
+        der = mergesort(a[mid:])
+        a = merge(izq,der)
+    return a
+def merge(izq,der):
+    i = 0
+    d = 0
+    n = len(izq)
+    m = len(der)
+    nueva = []
+    while i < n and d < m:
+        if izq[i] < der[d]:
+            nueva += [izq[i]]
+            i +=1
+        else:
+            nueva += [der[d]]
+            d += 1
+    if i < n:
+        while i < n:
+            nueva += [izq[i]]
+            i += 1
+    if d < m:
+        while d < m:
+            nueva += [der[d]]
+            d += 1
+    return nueva
 def main():
-    print('Secuencia de números separados por estacio!')
-    x = list(map(int,stdin.readline().strip().split(' ')))
-    res = quick_sort(x)
+    instanteini = datetime.now()
+    x = [1,5,3,8,10,22,48,2,1,0,3,6,9]
     if len(x) <= 10:
         print(x)
     else:
+        res = mergesort(x)
+        res.reverse()
         print(res[:10])
+    instantefin = datetime.now()
+    time = instantefin - instanteini
+    segundos = time.seconds
+    print(segundos)
 main()
     
