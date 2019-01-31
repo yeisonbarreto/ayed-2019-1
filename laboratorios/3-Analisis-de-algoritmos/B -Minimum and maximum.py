b6c800196ddd2b6d31a192782668764a235d6b0d
unknown <2160804@labinfo.is.escuelaing.edu.co>
2019-01-31 07:38:33 -0500

    B -Minimum and maximum

---
+++ laboratorios/3-Analisis-de-algoritmos/B -Minimum and maximum.py
@@ -0,0 +1,25 @@
+from sys import stdin
+def quick_sort(lista):
+    menor = []
+    mayor = []
+    if len(lista) == 0:
+        return []
+    else:
+        pivote = lista[0]
+        for i in range(1,len(lista)):
+            if lista[i] < pivote:
+                menor.append(lista[i])
+            else:
+                mayor.append(lista[i])
+    return quick_sort(mayor) + [pivote] + quick_sort(menor)
+def main():
+    print('Secuencia de números separados por estacio!')
+    x = list(map(int,stdin.readline().strip().split(' ')))
+    res = quick_sort(x)
+    if len(x) <= 1:
+        print(x)
+    else:
+        r = res[-1] , res[0]
+        print(r)
+main()
+    
